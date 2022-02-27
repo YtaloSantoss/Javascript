42ce8ca10af1b217fced10d0ba0f4ec8af6d3df9
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-02-26 21:14:03 -0300

    26/02/22

---
+++ aula13/EstruturasDeRepetição.js
@@ -0,0 +1,28 @@
+
+//Faça isso 
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+//------------------------------------------------
+
+
+// Enquanto isso
+var c = 1 
+while ( c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+*/
+
+//  Para 
+
+console.log('Vai comecar...')
+for (var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
