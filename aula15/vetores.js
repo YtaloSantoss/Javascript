aac40a2da21e36ee1242c7754ac8a18d1a31c95b
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-03-01 21:00:27 -0300

    01/03/22

---
+++ aula15/vetores.js
@@ -0,0 +1,8 @@
+
+var n =  [5, 8, 2, 9, 3]
+console.log(`Ordem crescente ${n.sort()}`)
+n.push(100)
+console.log(`O vetor tem ${n.length} posições`)
+console.log(`O primeiro valor do vetor é ${n[0]}`)
+console.log(n)
+
---
+++ aula15/vetores2.js
@@ -0,0 +1,8 @@
+var valores = [8, 1, 7, 4, 2, 9]
+//para pos=0 for menor que comprimento, acrescente mais um.
+for(var pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem valor ${valores[pos]}`)
+}
+
+
+
