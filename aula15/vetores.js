c80776d721fd77aa2fe1da5d5e8b7aade29955b3
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-03-01 21:23:27 -0300

    01/03/22

--- aula15/vetores.js
+++ aula15/vetores.js
@@ -4,5 +4,9 @@ console.log(`Ordem crescente ${n.sort()}`)
 n.push(100)
 console.log(`O vetor tem ${n.length} posições`)
 console.log(`O primeiro valor do vetor é ${n[0]}`)
-console.log(n)
-
+var teste = n.indexOf(4)
+if (teste== -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`Ovalor está na posição ${teste}`)
+}
---
+++ aula15/vetores3.js
@@ -0,0 +1,5 @@
+var valores = [8, 1, 7, 4, 2, 9]
+//Para cada variavel "posição" dentro de valores acrescente um 
+for (var pos in valores){
+    console.log(`A posição ${pos} tem valores ${valores[pos]}`)
+}
