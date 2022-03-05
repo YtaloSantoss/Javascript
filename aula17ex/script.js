cc4744a67ef857c76024f3ac16ef6d5fe4042d37
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-03-05 18:31:36 -0300

    05/03/22

--- aula17ex/script.js
+++ aula17ex/script.js
@@ -25,8 +25,40 @@ function adicionar(){
 
 //-----SE  for um numero adicionado "e" se "não" estiver em lista----
     if(Numero(input1.value) && !inLista(input1.value, valores)) {
-        window.alert('Tudo OK!')
+        valores.push(Number(input1.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${input1.value} adicionado.`
+        lista.appendChild(item)
+        saida.innerHTML =''
     }else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    input1.value=''
+    input1.focus()
+}
+
+function finalizar(){
+        if(valores.length == 0) {
+            window.alert('Adicione valores antes de finalizar!')
+        } else {
+            var total = valores.length
+            var maior = valores[0]
+            var menor = valores[0]
+            var soma = 0
+            var media = 0 
+            for(var pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior)
+                    maior = valores[pos]
+                if (valores[pos] < menor)
+                    menor = valores[pos]
+            }
+            media = soma / total
+            saida.innerHTML =''
+            saida.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+            saida.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+            saida.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+            saida.innerHTML +=`<p>Somando todos os valores, temos ${soma}.</.p>`
+            saida.innerHTML +=`<p>A média dos valores digitados é ${media}.</p>`
+        }
 }
